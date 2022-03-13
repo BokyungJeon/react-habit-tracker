1eab7d00b73b715651f29beb68bcccf63183322b
BokyungJeon <jjewell@naver.com>
2022-03-13 21:47:45 +0900

    test with react testing-library

--- src/components/habitAddForm.jsx
+++ src/components/habitAddForm.jsx
@@ -14,7 +14,6 @@ const HabitAddForm = memo((props) => {
     event.preventDefault();
     const name = inputRef.current.value;
     name && props.onAdd(name);
-    // inputRef.current.value = "";
     formRef.current.reset();
   };
   return (
@@ -23,7 +22,7 @@ const HabitAddForm = memo((props) => {
         ref={inputRef}
         type="text"
         className="add-input"
-        placeholder="Please Add Your Habit"
+        placeholder="Habit"
       />
       <button className="add-button">Add</button>
     </form>
@@ -31,35 +30,3 @@ const HabitAddForm = memo((props) => {
 });
 
 export default HabitAddForm;
-
-// // PureComponent는 최상위 데이터가 변하지 않으면 render함수가 호출되지 않음. 즉 re-rendering을 하지 않음.
-// class HabitAddForm extends PureComponent {
-//   // 리액트에서는 Dom요소를 직접적으로 쓰지 않기 때문에 리액트에서 다른 리액트 요소에 접근하고 싶다면 Ref를 쓴다.
-//   formRef = React.createRef();
-//   inputRef = React.createRef();
-
-//   onSubmit = (event) => {
-//     // 브라우저는 기본적으로 submit이 되면 refresh됨 이것을 막기 위해 event.preventDefault();
-//     event.preventDefault();
-//     // console.log(this.inputRef.current.value);
-//     const name = this.inputRef.current.value;
-//     name && this.props.onAdd(name);
-//     this.formRef.current.reset();
-//   };
-//   render() {
-//     console.log('habitAddForm');
-//     return (
-//       <form className="add-form" onSubmit={this.onSubmit}>
-//         <input
-//           ref={this.inputRef}
-//           type="text"
-//           className="add-input"
-//           placeholder="Please Add Your Habit"
-//         />
-//         <button className="add-button">Add</button>
-//       </form>
-//     );
-//   }
-// }
-
-// export default HabitAddForm;
